22cd66dfd8ba79e95146b62d35243b38fdcc3f9b
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-08-13 16:05:30 +0700

    refactor: install go, rust as system packages
    GitOrigin-RevId: efe2e866404344fc896c6c14e2c99375cf778800

--- dev-env/installers/go.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-VERSION=1.20
-
-wget -q https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
-tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz
-rm go${VERSION}.linux-amd64.tar.gz
