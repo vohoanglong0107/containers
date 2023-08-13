364d4e2a6b666cfbc9df86ae6357ae5c7f0150ee
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-08-13 16:10:02 +0700

    perf: install tektoncd cli as binaries
    Instead of rpm, it is way too slow to revalidate rpm repo cache
    
    GitOrigin-RevId: caf9f3b3bcc8a4f2195f126509cefda2df5edfce

--- dev-env/installers/tektoncd_cli.sh
+++ dev-env/installers/tektoncd_cli.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 VERSION=0.30.0
 URL=https://github.com/tektoncd/cli/releases/download/v${VERSION}
 
-wget -q ${URL}/tektoncd-cli-${VERSION}_Linux-64bit.rpm
+wget -q ${URL}/tkn_${VERSION}_Linux_x86_64.tar.gz 
 wget -q ${URL}/checksums.txt
 sha256sum --check --ignore-missing <checksums.txt
-dnf -y install tektoncd-cli-${VERSION}_Linux-64bit.rpm
-rm checksums.txt tektoncd-cli-${VERSION}_Linux-64bit.rpm
-dnf clean all
+tar -C /usr/local/bin/ -xzvf tkn_${VERSION}_Linux_x86_64.tar.gz tkn
+chmod +x /usr/local/bin/tkn
+rm checksums.txt tkn_${VERSION}_Linux_x86_64.tar.gz 
