107276fef228a7d9d790fe4b90246acc8c9fa52d
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-08-16 12:54:58 +0700

    chore: bump lazygit version

--- dev-env/installers/lazygit.sh
+++ dev-env/installers/lazygit.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-VERSION=v0.38.2
-URL=https://github.com/jesseduffield/lazygit/releases/download/$VERSION
+VERSION=0.40.2
+URL=https://github.com/jesseduffield/lazygit/releases/download/v$VERSION
 
-wget -q $URL/lazygit_0.38.2_Linux_x86_64.tar.gz
+wget -q $URL/lazygit_${VERSION}_Linux_x86_64.tar.gz
 wget -q $URL/checksums.txt
 sha256sum --check --ignore-missing <checksums.txt
-tar -C /usr/local/bin -xzf lazygit_0.38.2_Linux_x86_64.tar.gz lazygit
-rm checksums.txt lazygit_0.38.2_Linux_x86_64.tar.gz
+tar -C /usr/local/bin -xzf lazygit_${VERSION}_Linux_x86_64.tar.gz lazygit
+rm checksums.txt lazygit_${VERSION}_Linux_x86_64.tar.gz
