2fddc5f3fac02f0f49d8d1914b9a79b65d498890
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-08-17 20:11:45 +0700

    refactor: install system packages in os

---
+++ os/packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
+
+rpm-ostree override remove libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg \
+
+rpm-ostree install $(tr '\n' ' ' <"${SCRIPT_DIR}/packages.txt")
+
+rpm-ostree cleanup -m
