92fe35f8a8f75dda5da71d8e4af5aba4df7175b1
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-08-22 18:35:16 +0700

    fix: race condition on mktemp
    When quoted with double ticks, `mktemp -d` executed in the current command, making all xargs command run
    in the same tempdir. While quoted with single quote, `mktemp -d` is executed in a subshell by xargs for
    each find result

--- dev-env/installers/installers.sh
+++ dev-env/installers/installers.sh
@@ -7,11 +7,11 @@ find ${SCRIPT_DIR} \
   -maxdepth 1 \
   -type f \
   -regex '.*[0-9][0-9]_[a-z]+.sh' \
-  -print0 | sort -z | xargs -r -P 0 -0I% bash -c "cd `mktemp -d`; %;"
+  -print0 | sort -z | xargs -r -P 1 -0I% bash -c 'cd `mktemp -d`; %;'
 
 find ${SCRIPT_DIR} \
   -maxdepth 1 \
   -type f \
   ! -name 'installers.sh' \
   ! -regex '.*[0-9][0-9]_[a-z]+.sh' \
-  -print0 | xargs -r -P 0 -0I% bash -c "cd `mktemp -d`; %;"
+  -print0 | xargs -r -P 0 -0I% bash -c 'cd `mktemp -d`; %;'
