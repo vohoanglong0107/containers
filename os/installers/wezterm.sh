c20e65f65936ba85be3f0d84df07c0f2f982da37
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-07-28 13:22:47 +0900

    feat(os): add checksum to wezterm installer

--- os/installers/wezterm.sh
+++ os/installers/wezterm.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget -qO wezterm https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
-chmod +x wezterm
-mv wezterm /usr/bin
+VERSION=20240203-110809-5046fc22
+FORMAT=Ubuntu20.04.AppImage
+
+wget -q https://github.com/wez/wezterm/releases/download/$VERSION/WezTerm-$VERSION-$FORMAT
+wget -q https://github.com/wez/wezterm/releases/download/$VERSION/WezTerm-$VERSION-$FORMAT.sha256
+
+sha256sum --check WezTerm-$VERSION-$FORMAT.sha256
+
+chmod +x WezTerm-$VERSION-$FORMAT
+mv WezTerm-$VERSION-$FORMAT /usr/bin/wezterm
