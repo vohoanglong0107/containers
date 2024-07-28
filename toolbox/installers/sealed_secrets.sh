1034535adb9f1902010d2021bb6aad325593b500
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-07-28 15:17:12 +0900

    chore(toolbox): remove cloud related tools

--- toolbox/installers/sealed_secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-VERSION=0.20.2
-
-wget -q https://github.com/bitnami-labs/sealed-secrets/releases/download/v${VERSION}/kubeseal-${VERSION}-linux-amd64.tar.gz
-tar -xvzf kubeseal-${VERSION}-linux-amd64.tar.gz kubeseal
-install -m 755 kubeseal /usr/local/bin/kubeseal
-rm kubeseal kubeseal-${VERSION}-linux-amd64.tar.gz
