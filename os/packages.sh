3b13c47365af37bad87a2cb35681921def1b9d9b
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-07-28 10:59:56 +0900

    feat(os): install ffmpeg-free instead of ffmpeg
    To avoid packages conflict
    https://discussion.fedoraproject.org/t/ffmpeg-in-silverblue/111962/2

--- os/packages.sh
+++ os/packages.sh
@@ -7,8 +7,6 @@ rpm-ostree install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
   "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
 
-rpm-ostree override remove libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg \
-
 rpm-ostree install $(tr '\n' ' ' <"${SCRIPT_DIR}/packages.txt")
 
 rpm-ostree cleanup -m
