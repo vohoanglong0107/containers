91dc5ef439dd03521912c74b7c2cf4b25ea53fd3
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-07-28 15:15:17 +0900

    chore(toolbox): remove bazel related tools
    I hardly use bazel anymore

--- toolbox/installers/bazel.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-VERSION=v1.16.0
-
-wget -qO /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/${VERSION}/bazelisk-linux-amd64
-chmod +x /usr/local/bin/bazel
--- toolbox/installers/buildifier.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-VERSION=v6.1.2
-
-wget -q https://github.com/bazelbuild/buildtools/releases/download/$VERSION/buildifier-linux-amd64
-mv buildifier-linux-amd64 /usr/local/bin/buildifier
-chmod +x /usr/local/bin/buildifier
